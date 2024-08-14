568ac807b2788b4bf5de2d95022a9e20ad65ddad
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-08-14 20:52:49 -0300

    feat: Add banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import "./App.css";
+
 import Header from "./assets/components/Header.js";
 import Menu from "./assets/components/Menu.js";
-import "./App.css";
+import Banner from "./assets/components/Banner.js";
 
 function App() {
     return (
         <div className="container">
             <Header />
             <Menu />
+            <Banner />
         </div>
     );
 }
