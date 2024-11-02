f3634f9eca971059722366b92dc9d955426f1e66
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-11-02 18:43:12 -0300

    fix: Menu refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,21 +7,6 @@ function Header() {
     return (
         <header>
             <img src="/logo.png" alt="Logo Pipoca Mania" className={styles.logo} />
-
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/notifications" className={({ isActive }) => (isActive ? "active" : "")}>
-                            <BsBell className={styles.icon} />
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/profile" className={({ isActive }) => (isActive ? "active" : "")}>
-                            <BsPersonCircle className={styles.icon} />
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
         </header>
     );
 }
