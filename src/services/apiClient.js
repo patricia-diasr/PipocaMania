7764c046d1581e3af453f2b393a4d8e90e103949
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-11-27 21:11:47 -0300

    Ajustando para funcionar após atualização da API TMDB

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
+const API_BEARER_TOKEN =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjIwNmRlYWUxNTY0NmU5MTY2YzVmMzMwYTUwOTk3MCIsIm5iZiI6MTczMjc1MTEzMS4xMTgxMTUyLCJzdWIiOiI2NjllZmQ1MmFkYzY2MGIzZmIyNWY4NzQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.kcH6crEkCx4xp1w4yIiSK8KUX-3MT1twI3SYHrOZW1M";
+
 export const apiMovies = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${API_BEARER_TOKEN}`,
     },
 });
 
