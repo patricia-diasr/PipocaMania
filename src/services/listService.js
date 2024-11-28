675250a67410375bfb5f209ac0b637be50d82091
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-11-27 21:13:01 -0300

    Ajustando para funcionar após mudanças na API

--- src/services/listService.js
+++ src/services/listService.js
@@ -1,11 +1,9 @@
 import { apiMovies } from "./apiClient";
 import { apiMovieTheater } from "./apiClient";
 
-const API_KEY = "df206deae15646e9166c5f330a509970";
-
 export async function getUpcomingMovies() {
     try {
-        const response = await apiMovies.get(`/movie/upcoming?api_key=${API_KEY}&language=pt-BR&region=BR`);
+        const response = await apiMovies.get(`/movie/upcoming?language=pt-BR&region=BR`);
         return response.data.results.slice(0, 4);
     } catch (error) {
         throw new Error("Error fetching upcoming movies");
